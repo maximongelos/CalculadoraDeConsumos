c44fd5feffe5ec97acc2532e4841a88fea1bf0d5
Maxi Mongelos <maximongelos@gmail.com>
2022-04-10 03:08:33 -0300

    consultando y mostrando resultados de API

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
 	pedido: [],
 };
 
+const categorias = {
+	1: 'Comida',
+	2: 'Bebida',
+	3: 'Postres',
+};
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -38,4 +44,52 @@ function guardarCliente() {
 	const modalFormulario = document.querySelector('#formulario');
 	const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
 	modalBootstrap.hide();
+
+	mostrarSecciones();
+
+	//* Obtener platos de la api de JSON-server
+	obtenerPlatos();
+}
+
+function mostrarSecciones() {
+	const seccionesOcultas = document.querySelectorAll('.d-none');
+	seccionesOcultas.forEach((seccion) => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatos() {
+	const url = 'http://localhost:4000/platillos';
+
+	fetch(url)
+		.then((respuesta) => respuesta.json())
+		.then((platos) => mostrarPlatos(platos))
+		.catch((error) => console.error(error));
+}
+
+function mostrarPlatos(platos) {
+	const contenido = document.querySelector('#platillos .contenido');
+
+	platos.forEach((plato) => {
+		// const {nombre, id, categoria, precio} = plato;
+
+		const row = document.createElement('div');
+		row.classList.add('row', 'py-3', 'border-top');
+
+		const nombre = document.createElement('div');
+		nombre.classList.add('col-md-4');
+		nombre.textContent = plato.nombre;
+
+		const precio = document.createElement('div');
+		precio.classList.add('col-md-3', 'fw-bold');
+		precio.textContent = `$${plato.precio}`;
+
+		const categoria = document.createElement('div');
+		categoria.classList.add('col-md-3');
+		categoria.textContent = categorias[plato.categoria];
+
+		row.appendChild(nombre);
+		row.appendChild(precio);
+		row.appendChild(categoria);
+
+		contenido.appendChild(row);
+	});
 }
